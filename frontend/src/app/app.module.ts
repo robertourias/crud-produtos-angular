834e35e499f0d8155041b56fa675760e4631a4e7
Roberto Nicoletti <roberto.urias@gmail.com>
2020-04-19 01:00:43 -0300

    coponente product update

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -31,6 +31,7 @@ import { MatSortModule } from '@angular/material/sort';
 
 import localePt from  '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 
 registerLocaleData(localePt);
 @NgModule({
@@ -45,7 +46,8 @@ registerLocaleData(localePt);
     ForDirective,
     ProductCreateComponent,
     ProductReadComponent,
-    ProductRead2Component
+    ProductRead2Component,
+    ProductUpdateComponent
   ],
   imports: [
     BrowserModule,
