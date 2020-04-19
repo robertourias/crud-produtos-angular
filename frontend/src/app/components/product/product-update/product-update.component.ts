b0b72b1af34c8d04956a540d0d5395d895974c16
Roberto Nicoletti <roberto.urias@gmail.com>
2020-04-19 16:22:19 -0300

    alterando um produto via component

--- frontend/src/app/components/product/product-update/product-update.component.ts
+++ frontend/src/app/components/product/product-update/product-update.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { HttpClient } from '@angular/common/http';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Product } from '../product.model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-product-update',
@@ -7,9 +12,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductUpdateComponent implements OnInit {
 
-  constructor() { }
+  product: Product = {
+    id: 0,
+    name: '',
+    price: null
+  };
+
+  constructor(
+    private productService: ProductService,
+    private http: HttpClient,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    const id = parseInt(this.route.snapshot.paramMap.get('id'));
+    this.productService.readById(id).subscribe(product => {
+      this.product = product;
+    });
+  }
+
+  updateProduct(): void {
+    this.productService.update(this.product).subscribe(product => {
+      this.productService.showMessage('Produto atualizado com sucesso!')
+      this.router.navigate(['/products']);
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products']);
   }
 
 }
